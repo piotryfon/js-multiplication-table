7d74e992ed9e32b1a4555feaa39fe740f5dbbbc2
piotryfon <pit.pit@wp.pl>
2018-09-05 18:04:08 +0200

    cleanings in js

--- main.js
+++ main.js
@@ -5,43 +5,43 @@ let input = document.querySelector("#input");
 let showTask = document.querySelector("#showTask");
 let showResultDiv = document.querySelector("#show-result-div");
 let start = document.querySelector("#start");
-let startEventListener = start.addEventListener("click", startApi);
+let startEventListener = start.addEventListener("click", randomizeTask);
 let checkBtn = document.querySelector("#check");
 let checkBtnEventListener = checkBtn.addEventListener("click", checkResult);
-let parseValue;
+let parsedValue;
 let result;
 
-function submitVal(){
-    if(input.value){
-        parseValue = parseInt(input.value);
-        console.log("Napisałeś: " + parseValue);
-     showResultDiv.innerHTML = `<h2>Napisałeś ${parseValue}</h2>`;
-                
-    }else{
-     showResultDiv.innerHTML = "<h2>Wpisz wartość i potwierdź przyciskiem obok!</h2>"
-    }   
-}
+function randomizeTask(){
 
-function startApi(){
     let a = getRandomNumberInRange(1, 10);
     let b = getRandomNumberInRange(1, 10);
     
     result = a * b;
     console.log("Wynik to: " + result)
     showTask.innerHTML = `${a} x ${b} =`
-    startEventListener = start.removeEventListener("click", startApi)
-    checkBtnEventListener = checkBtn.addEventListener("click", checkResult);
-    return result;
+    startEventListener = start.removeEventListener("click", randomizeTask)
+    checkBtnEventListener = checkBtn.addEventListener("click", checkResult);  
 }
 
+function submitVal(){
+
+    if(input.value){
+        parsedValue = parseInt(input.value);
+        console.log("Napisałeś: " + parsedValue);
+     showResultDiv.innerHTML = `<h2>Napisałeś ${parsedValue}</h2>`;
+                
+    }else{
+     showResultDiv.innerHTML = "<h2>Wpisz wartość i potwierdź przyciskiem obok!</h2>"
+    }   
+}
 
 function checkResult(){
     
-    if(result && parseValue){
+    if(result && parsedValue){
         
-        console.log("result: " + result + " value: " + parseValue)
-        console.log(result === parseValue)
-        if(result === parseValue){
+        console.log("result: " + result + " value: " + parsedValue)
+        console.log(result === parsedValue)
+        if(result === parsedValue){
          showResultDiv.innerHTML = "<h2>Bardzo dobrze!</h2>"
         }else {
          showResultDiv.innerHTML = `<h2>Źle, wynik to: ${result}</h2>`
@@ -49,8 +49,6 @@ function checkResult(){
 
     }else showResultDiv.innerHTML = `<h2>Naciśnij "Start" i wpisz prawidłowo wartość!</h2>`
 
-    startEventListener = start.addEventListener("click", startApi);
-    
-    checkBtnEventListener = checkBtn.removeEventListener("click", checkResult);
-    
+    startEventListener = start.addEventListener("click", randomizeTask);
+    checkBtnEventListener = checkBtn.removeEventListener("click", checkResult);  
 }
